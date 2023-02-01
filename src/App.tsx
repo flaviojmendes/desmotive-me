1eace8805482b94368d914a5d45cd5c503317937
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-02-01 17:15:23 -0300

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -12,43 +12,53 @@ function App() {
     setQuote(_.sample(quotes) || "");
   });
 
-  ReactGA.initialize("G-JQ5JTL1Q21");
+  ReactGA.initialize("#");
   ReactGA.send({
     hitType: "pageview",
     page: window.location.pathname + window.location.search,
   });
 
+  function refreshPage() {
+    window.location.reload();
+  }
+
   return (
+
     <>
       <div
         className="h-screen w-screen flex flex-col items-center p-10"
         style={{
           backgroundColor: 'black',
-          backgroundImage: `url(https://picsum.photos/800/600/?blur)`,
+          backgroundImage: `url(https://source.unsplash.com/random/800x600/?coffee)`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
+          backgroundBlendMode: "luminosity",
         }}
       >
-        <h1 className="text-5xl f-handwritten text-white t-outline">
-          desmotive.me
-        </h1>
-
-        <div className="text-center flex flex-col grow mx-4 md:mx-20 p-2 min-h-[100px]">
-          <h1 className="t-quote  m-auto text-white bg-black bg-opacity-50 rounded-md p-10 text-lg md:text-3xl">
-            {quote}
+        <div className="text-center flex flex-col gap-5 m-auto md:mx-20">
+          <h1 className="font-family t-quote text-white bg-black bg-opacity-90 rounded-md p-10 text-lg md:text-3xl">
+            "{quote}"
           </h1>
-          <div className="text-center">
-            <TwitterShareButton
-              className="m-auto mt-0 w-[220px] flex content-center space-x-2 align-middle"
-              title={`${quote} 
-              \nvia: `}
-              url={"https://desmotive.me"}
-            >
-              <TwitterIcon size={64} round /><span className="m-auto text-white f-handwritten">Compartilhe</span>
-            </TwitterShareButton>
+
+          <div className="mb-30">
+            <button className="bg-black rounded-md p-4 text-lg m-1 text-white update-button" onClick={refreshPage}>Quero me desmotivar mais</button>
           </div>
+
         </div>
+
+        <div className="bg-black bg-opacity-90 rounded-md p-2">
+          <TwitterShareButton
+            className="m-auto mt-0 flex content-center space-x-2 align-middle"
+            title={`"${quote}" 
+              \nvia: `}
+            url={"#"}
+          >
+            <TwitterIcon size={32} borderRadius={10} /><span className="m-auto text-white f-handwritten text-lg">Compartilhe para desmotivar mais devs.</span>
+          </TwitterShareButton>
+        </div>
+
+
       </div>
     </>
   );
